ee81c0e21cdb30b90387812be129135e3d7c27d7
taewoo <tary82504@berkeley.edu>
2020-11-09 11:27:37 -0700

    boilerplate code complete

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+
+const ENTRY_DIR = path.join(__dirname, '/client');
+const DIST_DIR = path.join(__dirname, '/public');
+
+module.exports = {
+  entry: ['@babel/polyfill', `${ENTRY_DIR}/index.jsx`],
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: ['react-hot-loader/babel',
+            ['@babel/plugin-transform-runtime',
+              {
+                regenerator: true,
+              },
+            ],
+          ],
+        },
+      },
+      {
+        test: /.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: {
+          loader: 'url-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.jsx', '.js'],
+  },
+  devServer: {
+    contentBase: DIST_DIR,
+    compress: true,
+    filename: 'bundle.js',
+    port: 9000,
+  },
+};

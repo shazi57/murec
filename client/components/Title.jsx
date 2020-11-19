6994067eed302ec8195ec4e96addd72adb4b5029
taewoo <tary82504@berkeley.edu>
2020-11-19 00:23:27 -0700

    rough drafted title page design

---
+++ client/components/Title.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Col } from 'react-bootstrap';
+import '../styles/title.css';
+
+const Title = () => {
+  let wow = 'wow';
+  return (
+    <Col>
+      <div className="title">MUREK</div>
+      <div className="subtitle">LEMME DJ YOU RIGHT</div>
+    </Col>
+  );
+};
+
+export default Title;

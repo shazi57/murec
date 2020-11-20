a7ed8b8c14f81128c80ea9ae07bb159939611044
taewoo <tary82504@berkeley.edu>
2020-11-20 14:55:58 -0700

    title page complete

--- client/components/Title.jsx
+++ client/components/Title.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import { Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
+import djbox from '../styles/assets/djbox.gif';
+import IconButton from '@material-ui/core/IconButton';
+import ArrowDropDownCircleIcon from '@material-ui/icons/ArrowDropDownCircle';
 import '../styles/title.css';
 
 const Title = () => {
-  let wow = 'wow';
+  const wow = 'wow';
   return (
     <Col>
-      <div className="title">MUREK</div>
-      <div className="subtitle">LEMME DJ YOU RIGHT</div>
+      <Row className="title-container">
+        <h1 className="title">MUREK</h1>
+        <img className="djbox" src={djbox} alt="loading..." />
+      </Row>
+      <Row className="subtitle">LEMME DJ YOU RIGHT</Row>
+      <Row className="button-container">
+        <IconButton>
+          <ArrowDropDownCircleIcon className="dropdownbutton" />
+        </IconButton>
+      </Row>
     </Col>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -27,18 +27,22 @@ module.exports = {
         },
       },
       {
-        test: /.css$/i,
-        exclude: /node_modules/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-            },
-          },
-        ],
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
       },
+      // {
+      //   test: /.css$/,
+      //   exclude: /node_modules/,
+      //   use: [
+      //     'style-loader',
+      //     {
+      //       loader: 'css-loader',
+      //       options: {
+      //         modules: true,
+      //       },
+      //     },
+      //   ],
+      // },
       {
         test: /\.(png|jpg|gif|woff|woff2|ttf)$/i,
         use: {
